268bd683fab808b1e7c4348240ffd455e475878f
Seth Fitzsimmons <seth@mojodna.net>
2014-05-28 15:52:58 -0700

    Preliminary info and copy commands

---
+++ bin/tl.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+"use strict";
+
+var fs = require("fs"),
+    path = require("path");
+
+var parser = require("nomnom"),
+    tilelive = require("tilelive-cache")(require("tilelive-streaming")(require("tilelive")));
+
+require("mbtiles").registerProtocols(tilelive);
+
+parser.options({
+  version: {
+    abbr: "v",
+    flag: true,
+    help: "Show version info",
+    callback: function() {
+      return "tl v" + require("../package.json").version;
+    }
+  }
+});
+
+var commandDir = path.join(__dirname, "..", "lib", "commands"); 
+
+fs.readdirSync(commandDir)
+  .forEach(function(x) {
+    require(path.join(commandDir, x))(parser, tilelive);
+  });
+
+parser.parse();
